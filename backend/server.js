4332c6e7c2af5b07182ab3a9b2d200136b86ac89
Almedin Agushi <77640112+Almedin-Agushi@users.noreply.github.com>
2026-08-27 23:46:37 +0200

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1991,30 +1991,8 @@ app.use(
 // =====================================================
 // START SERVER
 // =====================================================
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
-
-app.listen(
-  PORT,
-  () => {
-    console.log(
-      "===================================="
-    );
-
-    console.log(
-      `AI CV Screening API running on port ${PORT}`
-    );
-
-    console.log(
-      `http://localhost:${PORT}`
-    );
-
-    console.log(
-      `Active Job: ${activeJobCriteria.jobTitle}`
-    );
-
-    console.log(
-      "===================================="
-    );
-  }
-);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
