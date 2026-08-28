98280a3fc293b9909fddc118abd0aba6bd9c919a
Almedin Agushi <77640112+Almedin-Agushi@users.noreply.github.com>
2026-08-28 10:42:56 +0200

    update dashboard

--- frontend/src/page/Dashboard.jsx
+++ frontend/src/page/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://ai-cv-screening-backend.onrender.com";
 
 function Dashboard() {
   const [candidates, setCandidates] = useState([]);
